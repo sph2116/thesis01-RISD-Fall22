0609fdc64df5cc76565ae0ade7fb8050f8979807
sph2116 <31591569+sph2116@users.noreply.github.com>
2022-09-26 22:52:13 -0400

    first commit

---
+++ serena-in-the-works-v1/scroll.js
@@ -0,0 +1,40 @@
+const sections = document.querySelectorAll(".content");
+let percentage; 
+
+
+
+//Use JavaScript IntersectionObserver API
+//The entry is NOT the tag itself - it is a special object including data such as intersection ratio 
+//etnry.target is the tag itself
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0.4){
+            entry.target.classList.add("in-view");
+        }
+        
+        // else {
+        //     entry.target.classList.remove("in-view")
+        // }
+    })
+}, {
+    threshold: [0, 0.4, 1.0] //specifies at what point in the scroll the observer should execute the code itself
+}); //End observer
+
+//Add section tags as the paremeter for the observer
+sections.forEach(section => {
+    observer.observe(section)
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    const progressBarInner = document.querySelector('.progress-bar');
+
+    document.addEventListener('scroll', function(){
+        console.log('scrolling')
+        percentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+        console.log('percentage is ' + percentage)
+
+        progressBarInner.style.width = `${percentage}%`
+
+    })
+
+})//End dom content loaded
